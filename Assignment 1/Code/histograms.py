b25c100931cee15488d80165d4f5d3c97cdf97c6
Reece Jones <reece.jones131@gmail.com>
2020-09-14 19:10:35 +0800

    Assignment 1: completed task 1.

--- Assignment 1/Code/histograms.py
+++ Assignment 1/Code/histograms.py
@@ -16,15 +16,14 @@ HISTOGRAM_BINS = 16
 
 
 for file in [f for p in INPUT for f in glob(p)]:
-    image = cv.imread(file, cv.IMREAD_COLOR)
-    image_grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    image = cv.imread(file, cv.IMREAD_GRAYSCALE)
     filename = os.path.splitext(os.path.basename(file))[0]
 
     pyplot.clf()
-    pyplot.hist(image_grey.flatten(), HISTOGRAM_BINS, (0, 255))
+    pyplot.hist(image.flatten(), HISTOGRAM_BINS, (0, 255))
     pyplot.xlabel('Intensity')
     pyplot.ylabel('Frequency')
-    pyplot.ylim((0, image_grey.shape[0] * image_grey.shape[1]))
+    pyplot.ylim((0, image.shape[0] * image.shape[1]))
     pyplot.tight_layout()
     output_file = OUTPUT_FILE.format(filename)
     pyplot.savefig(output_file)
--- Assignment 1/Code/sift.py
+++ Assignment 1/Code/sift.py
@@ -12,14 +12,19 @@ INPUT = [
 ]
 HISTOGRAM_OUTPUT_FILE = 'results/{}-hist.png'
 OUTPUT_FILE = 'results/{}-keypoints.png'
+SCALE = 2
 
 for file in [f for p in INPUT for f in glob(p)]:
     image = cv.imread(file, cv.IMREAD_COLOR)
-    image_grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     filename = os.path.splitext(os.path.basename(file))[0]
 
     sift = cv.xfeatures2d.SIFT_create()
     keypoints = sift.detect(image, None)
-    result = cv.drawKeypoints(image.copy(), keypoints, None)
+    # Scale everything up so we can see it better (doesn't affect keypoint location)
+    for kp in keypoints:
+        kp.pt = (kp.pt[0] * SCALE, kp.pt[1] * SCALE)
+        kp.size *= SCALE
+    result = cv.resize(image, (0, 0), fx=SCALE, fy=SCALE)
+    result = cv.drawKeypoints(result, keypoints, None)
     output_file = OUTPUT_FILE.format(filename)
     cv.imwrite(output_file, result)
