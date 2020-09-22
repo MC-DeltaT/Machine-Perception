0d6e612b8351b23d2d5f02a1e0a1ce6e0bdd7920
Reece Jones <reece.jones131@gmail.com>
2020-09-23 00:13:26 +0800

    Assignment 1: more task 4.

--- Assignment 1/Code/t4_segmentation.py
+++ Assignment 1/Code/t4_segmentation.py
@@ -6,13 +6,13 @@ import os.path
 from pathlib import Path
 
 INPUTS = ['data/card.png', 'data/dugong.jpg']
-OUTPUT_DIR = 'results/{}'
-OUTPUT_FILE = '{}-segmentation.png'
-PERFORMANCE_FILE = 'segmentation_performance.txt'
+OUTPUT_DIR = 'results/{}/segmentation'
+OUTPUT_FILE = '{}.png'
+PERFORMANCE_FILE = 'performance.txt'
 KMEANS_CLUSTERS = 3
-KMEANS_ITERATIONS = 100
-KMEANS_EPSILON = 0.1
-KMEANS_ATTEMPTS = 10
+KMEANS_ITERATIONS = 200
+KMEANS_EPSILON = 0.01
+KMEANS_ATTEMPTS = 5
 SEGMENT_COLOURS = numpy.array([
     (255, 0, 0),
     (0, 255, 0),
@@ -25,27 +25,37 @@ for file in INPUTS:
     output_dir = OUTPUT_DIR.format(filename)
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-    with open(os.path.join(output_dir, PERFORMANCE_FILE), 'w') as performance_file:
-        features = [
-            ('greyscale', cv.cvtColor(image, cv.COLOR_BGR2GRAY)[:, :, numpy.newaxis]),
-            ('rgb', image),
-            ('hsv', cv.cvtColor(image, cv.COLOR_BGR2HSV)),
-            ('hls', cv.cvtColor(image, cv.COLOR_BGR2HLS)),
-            ('xyz', cv.cvtColor(image, cv.COLOR_BGR2XYZ)),
-            ('lab', cv.cvtColor(image, cv.COLOR_BGR2LAB)),
-            ('luv', cv.cvtColor(image, cv.COLOR_BGR2LUV)),
-            ('yuv', cv.cvtColor(image, cv.COLOR_BGR2YUV))
-        ]
+    features = [
+        ('greyscale', cv.cvtColor(image, cv.COLOR_BGR2GRAY)[:, :, numpy.newaxis]),
+        ('rgb', image),
+        ('hsv', cv.cvtColor(image, cv.COLOR_BGR2HSV)),
+        ('hls', cv.cvtColor(image, cv.COLOR_BGR2HLS)),
+        ('xyz', cv.cvtColor(image, cv.COLOR_BGR2XYZ)),
+        ('lab', cv.cvtColor(image, cv.COLOR_BGR2LAB)),
+        ('luv', cv.cvtColor(image, cv.COLOR_BGR2LUV)),
+        ('yuv', cv.cvtColor(image, cv.COLOR_BGR2YUV)),
+        ('red', image[:, :, 2][:, :, numpy.newaxis]),
+        ('green', image[:, :, 1][:, :, numpy.newaxis]),
+        ('blue', image[:, :, 0][:, :, numpy.newaxis]),
+        ('hue', cv.cvtColor(image, cv.COLOR_BGR2HSV)[:, :, 0][:, :, numpy.newaxis]),
+        ('saturation', cv.cvtColor(image, cv.COLOR_BGR2HSV)[:, :, 1][:, :, numpy.newaxis])
+    ]
 
-        for method_name, data in features:
-            height, width, feature_size = data.shape
-            data = data.reshape((width * height, feature_size)).astype(numpy.float32)
-            data = cv.normalize(data, None, 0, 1, cv.NORM_MINMAX)
-            kmeans_criteria = (cv.TERM_CRITERIA_MAX_ITER + cv.TERM_CRITERIA_EPS, KMEANS_ITERATIONS, KMEANS_EPSILON)
-            kmeans_flags = cv.KMEANS_RANDOM_CENTERS
-            compactness, best_labels, centres = cv.kmeans(data, KMEANS_CLUSTERS, None, kmeans_criteria, KMEANS_ATTEMPTS, kmeans_flags)
-            best_labels = best_labels.reshape((height, width))
-            result = SEGMENT_COLOURS[best_labels]
-            output_file = os.path.join(output_dir, OUTPUT_FILE.format(method_name))
-            cv.imwrite(output_file, result)
-            performance_file.write(f'{method_name}: {round(compactness)}\n')
+    method_performance = []
+    for method_name, data in features:
+        height, width, feature_size = data.shape
+        data = data.reshape((width * height, feature_size)).astype(numpy.float32)
+        data = (data - numpy.min(data)) / (numpy.max(data) - numpy.min(data))
+        kmeans_criteria = (cv.TERM_CRITERIA_MAX_ITER + cv.TERM_CRITERIA_EPS, KMEANS_ITERATIONS, KMEANS_EPSILON)
+        kmeans_flags = cv.KMEANS_RANDOM_CENTERS
+        compactness, best_labels, centres = cv.kmeans(data, KMEANS_CLUSTERS, None, kmeans_criteria, KMEANS_ATTEMPTS, kmeans_flags)
+        best_labels = best_labels.reshape((height, width))
+        result = SEGMENT_COLOURS[best_labels]
+        output_file = os.path.join(output_dir, OUTPUT_FILE.format(method_name))
+        cv.imwrite(output_file, result)
+        method_performance.append((method_name, compactness))
+
+    method_performance = sorted(method_performance, key=lambda t: t[1])
+    with open(os.path.join(output_dir, PERFORMANCE_FILE), 'w') as performance_file:
+        for method, compactness in method_performance:
+            performance_file.write(f'{method}: {round(compactness)}\n')
---
+++ Assignment 1/Code/t4_space_analysis.py
@@ -0,0 +1,72 @@
+import cv2 as cv
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
+import numpy
+import os.path
+from pathlib import Path
+
+INPUTS = ['data/card.png', 'data/dugong.jpg']
+OUTPUT_DIR = 'results/{}/segmentation'
+OUTPUT_FILE = '{}-space.png'
+
+def visualiser_1d(axis1):
+    def inner(data, output_file):
+        pyplot.clf()
+        pyplot.hist(data.ravel(), 100)
+        pyplot.xlabel(axis1)
+        pyplot.ylabel('Frequency')
+        pyplot.tight_layout()
+        pyplot.savefig(output_file)
+    return inner
+
+# def visualiser_2d(axis1, axis2):
+#     def inner(data, output_file):
+#         pyplot.clf()
+#         pyplot.scatter(data[:, 0], data[:, 1])
+#         pyplot.xlabel(axis1)
+#         pyplot.ylabel(axis2)
+#         pyplot.tight_layout()
+#         pyplot.savefig(output_file)
+#     return inner
+
+def visualiser_3d(axis1, axis2, axis3):
+    def inner(data, output_file):
+        pyplot.clf()
+        figure = pyplot.figure()
+        axes = figure.add_subplot(111, projection='3d')
+        axes.scatter(data[:, 0].ravel(), data[:, 1].ravel(), data[:, 2].ravel())
+        axes.set_xlabel(axis1)
+        axes.set_ylabel(axis2)
+        axes.set_zlabel(axis3)
+        pyplot.tight_layout()
+        pyplot.savefig(output_file)
+    return inner
+
+for file in INPUTS:
+    image = cv.imread(file, cv.IMREAD_COLOR)
+    filename = os.path.splitext(os.path.basename(file))[0]
+    output_dir = OUTPUT_DIR.format(filename)
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    features = [
+        ('greyscale', cv.cvtColor(image, cv.COLOR_BGR2GRAY)[:, :, numpy.newaxis], visualiser_1d('Intensity')),
+        ('rgb', image, visualiser_3d('Blue', 'Green', 'Red')),
+        ('hsv', cv.cvtColor(image, cv.COLOR_BGR2HSV), visualiser_3d('Hue', 'Saturation', 'Value')),
+        ('hls', cv.cvtColor(image, cv.COLOR_BGR2HLS), visualiser_3d('Hue', 'Lightness', 'Saturation')),
+        ('xyz', cv.cvtColor(image, cv.COLOR_BGR2XYZ), visualiser_3d('X', 'Y', 'Z')),
+        ('lab', cv.cvtColor(image, cv.COLOR_BGR2LAB), visualiser_3d('L', 'a', 'b')),
+        ('luv', cv.cvtColor(image, cv.COLOR_BGR2LUV), visualiser_3d('L', 'u', 'v')),
+        ('yuv', cv.cvtColor(image, cv.COLOR_BGR2YUV), visualiser_3d('Y', 'U', 'V')),
+        ('red', image[:, :, 2][:, :, numpy.newaxis], visualiser_1d('Red')),
+        ('green', image[:, :, 1][:, :, numpy.newaxis], visualiser_1d('Green')),
+        ('blue', image[:, :, 0][:, :, numpy.newaxis], visualiser_1d('Blue')),
+        ('hue', cv.cvtColor(image, cv.COLOR_BGR2HSV)[:, :, 0][:, :, numpy.newaxis], visualiser_1d('Hue')),
+        ('saturation', cv.cvtColor(image, cv.COLOR_BGR2HSV)[:, :, 1][:, :, numpy.newaxis], visualiser_1d('Saturation'))
+    ]
+
+    for name, data, visualiser in features:
+        data = data.reshape((data.shape[0] * data.shape[1], data.shape[2])).astype(numpy.float32)
+        data = (data - numpy.min(data)) / (numpy.max(data) - numpy.min(data))
+        data_samples = data[::4]
+        output_file = os.path.join(output_dir, OUTPUT_FILE.format(name))
+        visualiser(data_samples, output_file)
