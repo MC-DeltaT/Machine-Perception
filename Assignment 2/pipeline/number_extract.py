c9ea4c0082b0f7329f91d40a136cf51d37fdb475
Reece Jones <reece.jones131@gmail.com>
2020-10-16 15:27:30 +0800

    Assignment 2: improved number extraction.

--- Assignment 2/pipeline/number_extract.py
+++ Assignment 2/pipeline/number_extract.py
@@ -37,10 +37,16 @@ def detect_regions(image: numpy.ndarray) -> Sequence[BoundingBox]:
         if not REGION_ASPECT_RATIO_RANGE[0] <= w / h <= REGION_ASPECT_RATIO_RANGE[1]:
             continue
 
+        region_grey = image_grey[y:y + h, x:x + w]
+
         # Further processing and recognition assumes that the digit is the foreground (i.e. light on
         # a dark background) in the local area. But MSER can produce regions that are dark on a
         # light background, so need to filter out those.
-        if not _region_is_foreground(image_grey, box, points):
+        if not _region_is_foreground(region_grey, box, points):
+            continue
+
+        # Digits typically only have 1 main foreground component.
+        if _has_multiple_components(region_grey):
             continue
 
         # Filters out regions that are sub-regions of other regions (occurs very often with MSER).
@@ -89,23 +95,31 @@ def select_number(image: numpy.ndarray, boxes: Sequence[BoundingBox]) -> Sequenc
     box_heights = [numpy.array([b[3] for b in p]) for p in paths]
     box_height_means = numpy.array([numpy.mean(hs) for hs in box_heights])
 
-    # path_filter = numpy.zeros(len(paths), numpy.bool)
-    if False: #numpy.all(path_filter):
-        return []
-    else:
-        best_path = numpy.argmax(path_lengths)
-        return paths[best_path]
+    path_filter = numpy.zeros(len(paths), numpy.bool)
+    # Consider only the paths with the tallest boxes.
+    path_filter[box_height_means / numpy.max(box_height_means) < NUMBER_TALL_THRESHOLD] = True
+    # Then select the longest path.
+    best_path = numpy.ma.argmax(numpy.ma.masked_array(path_lengths, path_filter))
+    return paths[best_path]
 
 
 # Checks if a significant amount of pixels in a region are foreground when considering the local area.
-def _region_is_foreground(image_grey: numpy.ndarray, box: BoundingBox, points: numpy.ndarray) -> bool:
-    x, y, w, h = box
-    region_grey = image_grey[y:y + h, x:x + w]
+def _region_is_foreground(region_grey: numpy.ndarray, box: BoundingBox, points: numpy.ndarray) -> bool:
     _, binary = cv.threshold(region_grey, 0, 1, cv.THRESH_OTSU)
-    indices = numpy.flip(points - [x, y], 1)
+    indices = numpy.flip(points - [box[0], box[1]], 1)
     fg_points = binary.ravel()[numpy.ravel_multi_index((indices[:, 0], indices[:, 1]), binary.shape)]
     region_fg_ratio = numpy.average(fg_points)
     return region_fg_ratio >= REGION_FG_RATIO_THRESHOLD
+
+
+# Checks if there are multiple (significant) foreground components.
+def _has_multiple_components(region_grey: numpy.ndarray) -> bool:
+    _, binary = cv.threshold(region_grey, 0, 255, cv.THRESH_OTSU)
+    _, labels = cv.connectedComponents(binary)
+    fg_component_areas = numpy.bincount(labels.ravel())[1:]
+    max_area = numpy.max(fg_component_areas)
+    significant_components = fg_component_areas / max_area > REGION_SIGNIFICANT_COMPONENT_AREA_THRESHOLD
+    return numpy.count_nonzero(significant_components) > 1
 
 
 # Checks if a region's foreground is part of a larger object.
@@ -129,19 +143,14 @@ def _is_subregion(image_grey: numpy.ndarray, box: BoundingBox) -> Tuple[bool, Bo
                 y2 = min(y2 + axis[1], image_grey.shape[0])
 
             region_grey = image_grey[y1:y2, x1:x2]
-            region_grey = cv.normalize(region_grey, None, 0, 255, cv.NORM_MINMAX)
 
             _, binary = cv.threshold(region_grey, 0, 1, cv.THRESH_OTSU)
-            # Find label of foreground point closest the to centre of the region.
-            fg_indices = numpy.argwhere(binary)
-            centre_index = (binary.shape[0] / 2, binary.shape[1] / 2)
-            fg_centre_index = fg_indices[
-                numpy.argmin(numpy.linalg.norm(fg_indices - centre_index, axis=1))]
+            # Find the label of the main foreground component.
             _, labels = cv.connectedComponents(binary)
-            centre_label = labels[fg_centre_index[0], fg_centre_index[1]]
-            assert centre_label > 0
+            fg_component_areas = numpy.bincount(labels.ravel())[1:]
+            fg_label = numpy.argmax(fg_component_areas) + 1
 
-            # Check if the centre foreground component touches the border of the box.
+            # Check if the main foreground component touches the border of the box.
             border_mask = numpy.zeros_like(region_grey, numpy.bool)
             if direction < 0 and axis[0]:
                 border_mask[:, 0] = True
@@ -152,7 +161,7 @@ def _is_subregion(image_grey: numpy.ndarray, box: BoundingBox) -> Tuple[bool, Bo
             elif direction > 0 and axis[1]:
                 border_mask[-1, :] = True
             border_labels = labels[border_mask]
-            if numpy.all(border_labels != centre_label):
+            if numpy.all(border_labels != fg_label):
                 # Not subregion is this axis and direction.
                 # Revert box so we don't get a 1 pixel gap around the foreground.
                 x1 = prev_x1
@@ -235,10 +244,12 @@ def _boxes_intersecting_line(line: Line, boxes: Sequence[BoundingBox]) -> Sequen
     return list(intersecting_boxes)
 
 
-REGION_AREA_MIN = 50
-REGION_AREA_MAX = 0.5 * 0.5
+REGION_AREA_MIN = 50        # Pixels
+REGION_AREA_MAX = 0.5 * 0.5     # Fraction of image area
 REGION_ASPECT_RATIO_RANGE = (0.2, 0.8)
 REGION_FG_RATIO_THRESHOLD = 0.75
-REGION_GROWTH = 5
-EQUIVALENT_BOX_DISTANCE = 5
+REGION_SIGNIFICANT_COMPONENT_AREA_THRESHOLD = 0.05   # Fraction of largest component's area
+REGION_GROWTH = 5       # Pixels
+EQUIVALENT_BOX_DISTANCE = 5     # Pixels
 NUMBER_LINE_ANGLE_THRESHOLD = radians(20)
+NUMBER_TALL_THRESHOLD = 0.75    # Fraction of tallest boxes' height
